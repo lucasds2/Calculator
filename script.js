6921b1a0d7ea5eee68c349fc645d862a2441870a
lucasds2 <lucassantos1662@gmail.com>
2021-09-17 19:11:06 -0300

    update

--- script.js
+++ script.js
@@ -55,8 +55,12 @@ function inputNumbersKeyboard(){
     }
     else if(event.key === "="){
       calcOperation()
-    }else{
-      alert("Apenas números e símbolos")
+    }
+    else if(event.key === "Shift" || "Alt"){
+      return "ok"
+    }
+    else{
+      alert("Apenas números e símbolos") 
     }
   })
   
@@ -82,108 +86,6 @@ function calcOperation(){
 
 window.addEventListener("load", inputNumbersKeyboard())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if(number1){
-//   function putNumber1(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number1.addEventListener("click", putNumber1)
-// }
-
-// if(number2){
-//   function putNumber2(){
-//     result.innerHTML += number2.textContent
-//   }
-//   number2.addEventListener("click", putNumber1)
-// }
-
-// if(number3){
-//   function putNumber3(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number3.addEventListener("click", putNumber1)
-// }
-
-// if(number4){
-//   function putNumber4(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number4.addEventListener("click", putNumber1)
-// }
-
-// if(number5){
-//   function putNumber5(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number5.addEventListener("click", putNumber1)
-// }
-
-// if(number6){
-//   function putNumber6(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number6.addEventListener("click", putNumber1)
-// }
-
-// if(number7){
-//   function putNumber7(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number7.addEventListener("click", putNumber1)
-// }
-
-// if(number8){
-//   function putNumber8(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number8.addEventListener("click", putNumber1)
-// }
-
-// if(number9){
-//   function putNumber9(){
-//     result.innerHTML += number1.textContent
-//   }
-//   number9.addEventListener("click", putNumber1)
-// }
 
 
   
