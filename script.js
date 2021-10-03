99b157ede639d31eaedc1361b78680301f1760e4
lucasds2 <lucassantos1662@gmail.com>
2021-10-02 21:36:53 -0300

    melhorando o código

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 
-function inputNumbers(number){
-  let num = document.querySelector(".result").innerHTML
-  document.querySelector(".result").innerHTML = num + number
-}
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".sub-operators img")
+const numbersArray = Array.from(numbers)
+numbersArray.forEach((number) => {
+  number.addEventListener("click", () => {
+    let value = number.innerText
+    document.querySelector(".result").innerHTML += value
+  })
+})
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    let operatorValue = operator.id
+    document.querySelector(".result").innerHTML += operatorValue
+  })
+})
 
 function inputNumbersKeyboard(){
   window.document.addEventListener("keydown", function(event){
     let transformNumber = Number(event.key)
     let isNumber = transformNumber || event.key === Number
-    let num = document.querySelector(".result").innerHTML
     if(isNumber){
-    document.querySelector(".result").innerHTML = num + transformNumber
+    document.querySelector(".result").innerHTML += transformNumber
     }
     else if (event.key === "0"){
-      document.querySelector('.result').innerHTML = num + transformNumber
+      document.querySelector('.result').innerHTML += transformNumber
     }
     else if (event.key === "+"){
-      document.querySelector('.result').innerHTML = num + event.key
+      document.querySelector('.result').innerHTML += event.key
     }
     else if (event.key === "-"){
-      document.querySelector('.result').innerHTML = num + event.key
+      document.querySelector('.result').innerHTML += event.key
     }
     else if (event.key === "*"){
-      document.querySelector('.result').innerHTML = num + event.key
+      document.querySelector('.result').innerHTML += event.key
     }
     else if (event.key === "/"){
-      document.querySelector('.result').innerHTML = num + event.key
+      document.querySelector('.result').innerHTML += event.key
     }
     else if(event.key === "Backspace"){
       backspace()
@@ -39,8 +49,6 @@ function inputNumbersKeyboard(){
   })
   
 }
-
-
 
 function cleanAll(){
   document.querySelector(".result").innerHTML = ""
