b7ea5e9b3f46bf2582b5716d59ac7c58d39dd4bf
lucasds2 <lucassantos1662@gmail.com>
2022-01-25 21:45:51 -0300

    changelog 1.0: changing the code part

--- script.js
+++ script.js
@@ -1,69 +1,63 @@
-
 const numbers = document.querySelectorAll(".number")
 const operators = document.querySelectorAll(".sub-operators img")
-const numbersArray = Array.from(numbers)
-numbersArray.forEach((number) => {
-  number.addEventListener("click", () => {
-    let value = number.innerText
-    document.querySelector(".result").innerHTML += value
-  })
-})
-operators.forEach((operator) => {
-  operator.addEventListener("click", () => {
-    let operatorValue = operator.id
-    document.querySelector(".result").innerHTML += operatorValue
-  })
-})
 
-function inputNumbersKeyboard(){
-  window.document.addEventListener("keydown", function(event){
-    let transformNumber = Number(event.key)
-    let isNumber = transformNumber || event.key === Number
-    if(isNumber){
-    document.querySelector(".result").innerHTML += transformNumber
-    }
-    else if (event.key === "0"){
-      document.querySelector('.result').innerHTML += transformNumber
-    }
-    else if (event.key === "+"){
-      document.querySelector('.result').innerHTML += event.key
-    }
-    else if (event.key === "-"){
-      document.querySelector('.result').innerHTML += event.key
-    }
-    else if (event.key === "*"){
-      document.querySelector('.result').innerHTML += event.key
-    }
-    else if (event.key === "/"){
-      document.querySelector('.result').innerHTML += event.key
-    }
-    else if(event.key === "Backspace"){
-      backspace()
-    }
-    else if(event.key === "Enter"){
-      calcOperation()
-    }
-    else{
-      return("Apenas números e operadores")
-    }
+for (let i = 0; i < numbers.length; i++) {
+  const isNumber = numbers[i]
+  const valueOfNumbers = Number(isNumber.innerText)
+  if (i <= 3) {
+    const isOperators = operators[i]
+    const idOfOperators = isOperators.id
+    isOperators.addEventListener("click", () => {
+      divResult(idOfOperators)
+    })
+  }
+  isNumber.addEventListener("click", () => {
+    divResult(valueOfNumbers)
+    console.log(valueOfNumbers)
   })
-  
 }
 
-function cleanAll(){
+
+function inputNumbersKeyboard() {
+  window.document.addEventListener("keydown", function (user) {
+    let arrayWithValues = []
+    arrayWithValues.push(user.key)
+    let isNumber = isNaN(user.key)
+    let isOperators = arrayWithValues[0] === "+" || arrayWithValues[0] === "-" || arrayWithValues[0] === "*" || arrayWithValues[0] === "/"
+    if (!isNumber) {
+      divResult(user.key)
+    } else if (isOperators) {
+      divResult(user.key)
+    } else if (user.key === "Backspace") {
+      backspace()
+    } else if (user.key === "Enter") {
+      calcOperation()
+    } else {
+      return ("Apenas números e operadores")
+    }
+  })
+
+}
+
+function cleanAll() {
   document.querySelector(".result").innerHTML = ""
 }
 
-function backspace(){
+function backspace() {
   let result = document.querySelector(".result").innerHTML
-  document.querySelector('.result').innerHTML = result.substring(0, result.length -1)
+  document.querySelector('.result').innerHTML = result.substring(0, result.length - 1)
 }
 
-function calcOperation(){
+function calcOperation() {
   let resultInnerHTMl = document.querySelector(".result").innerHTML
-  if(resultInnerHTMl){
+  if (resultInnerHTMl) {
     document.querySelector(".result").innerHTML = eval(resultInnerHTMl)
+    console.log(document.querySelector(".result").innerHTML = eval(resultInnerHTMl))
   }
+}
+
+function divResult(value) {
+  document.querySelector(".result").innerHTML += value
 }
 
 window.addEventListener("load", inputNumbersKeyboard())
