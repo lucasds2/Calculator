8c5530f11d4ef15ed69609d0c9550b582856822a
lucasds2 <lucassantos1662@gmail.com>
2022-03-01 19:32:26 -0300

    Fix: Re-mapping things

--- script.js
+++ script.js
@@ -1,63 +1,101 @@
-const numbers = document.querySelectorAll(".number")
-const operators = document.querySelectorAll(".sub-operators img")
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+let arr = [];
 
 for (let i = 0; i < numbers.length; i++) {
-  const isNumber = numbers[i]
-  const valueOfNumbers = Number(isNumber.innerText)
+  const isNumber = numbers[i];
+  const valueOfNumbers = isNumber.innerText;
   if (i <= 3) {
-    const isOperators = operators[i]
-    const idOfOperators = isOperators.id
+    const isOperators = operators[i];
+    const valueOfOperators = isOperators.innerText;
     isOperators.addEventListener("click", () => {
-      divResult(idOfOperators)
-    })
+      divResult(valueOfOperators);
+    });
   }
   isNumber.addEventListener("click", () => {
-    divResult(valueOfNumbers)
-    console.log(valueOfNumbers)
-  })
+    divResult(valueOfNumbers);
+  });
 }
 
-
-function inputNumbersKeyboard() {
+function keyboardInput() {
   window.document.addEventListener("keydown", function (user) {
-    let arrayWithValues = []
-    arrayWithValues.push(user.key)
-    let isNumber = isNaN(user.key)
-    let isOperators = arrayWithValues[0] === "+" || arrayWithValues[0] === "-" || arrayWithValues[0] === "*" || arrayWithValues[0] === "/"
+    let arrayWithValues = [];
+    arrayWithValues.push(user.key);
+    let isNumber = isNaN(user.key);
+    let isOperators =
+      arrayWithValues[0] === "+" ||
+      arrayWithValues[0] === "-" ||
+      arrayWithValues[0] === "*" ||
+      arrayWithValues[0] === "/";
     if (!isNumber) {
-      divResult(user.key)
+      divResult(user.key);
     } else if (isOperators) {
-      divResult(user.key)
+      divResult(user.key);
     } else if (user.key === "Backspace") {
-      backspace()
+      buttons.backspaceKeyboard();
     } else if (user.key === "Enter") {
-      calcOperation()
+      calcOperation();
     } else {
-      return ("Apenas números e operadores")
+      return "Apenas números e operadores";
     }
-  })
-
+  });
 }
 
-function cleanAll() {
-  document.querySelector(".result").innerHTML = ""
+function numberWithCommas(value) {
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function backspace() {
-  let result = document.querySelector(".result").innerHTML
-  document.querySelector('.result').innerHTML = result.substring(0, result.length - 1)
-}
+class Buttons {
+  constructor(buttonClear, buttonBackspace, buttonCalc) {
+    this.buttonClear = buttonClear;
+    this.buttonBackspace = buttonBackspace;
+    this.buttonCalc = buttonCalc;
+    this.resultInner = document.querySelector(".result");
+  }
 
-function calcOperation() {
-  let resultInnerHTMl = document.querySelector(".result").innerHTML
-  if (resultInnerHTMl) {
-    document.querySelector(".result").innerHTML = eval(resultInnerHTMl)
-    console.log(document.querySelector(".result").innerHTML = eval(resultInnerHTMl))
+  clearAll() {
+    this.buttonClear.addEventListener("click", () => {
+      this.resultInner.innerHTML = "";
+    });
+  }
+
+  backspace() {
+    this.buttonBackspace.addEventListener("click", () => {
+      let resultHTML = this.resultInner.innerHTML;
+      this.resultInner.innerHTML = resultHTML.substring(
+        0,
+        resultHTML.length - 1
+      );
+    });
+  }
+
+  backspaceKeyboard() {
+    let resultHTML = this.resultInner.innerHTML;
+    this.resultInner.innerHTML = resultHTML.substring(0, resultHTML.length - 1);
+  }
+
+  calcOperation() {
+    this.buttonCalc.addEventListener("click", () => {
+      if (this.resultInner.innerHTML !== "") {
+        this.resultInner.innerHTML = numberWithCommas(
+          eval(this.resultInner.innerHTML)
+        );
+      }
+    });
   }
 }
 
+let buttonClear = document.querySelector(".clear-all");
+let buttonBackspace = document.querySelector(".backspace");
+let buttonCalc = document.querySelector(".calc");
+
+const buttons = new Buttons(buttonClear, buttonBackspace, buttonCalc);
+buttons.clearAll();
+buttons.backspace();
+buttons.calcOperation();
+
 function divResult(value) {
-  document.querySelector(".result").innerHTML += value
+  document.querySelector(".result").innerHTML += value;
 }
 
-window.addEventListener("load", inputNumbersKeyboard())
+window.addEventListener("load", keyboardInput());
